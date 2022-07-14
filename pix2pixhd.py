f5a36669da0834d4b78eeb8c090df69f284d5f41
luis96920 <lucasm3938@gmail.com>
2022-07-14 17:56:35 -0300

    Add argsParser. v1

---
+++ pix2pixhd.py
@@ -0,0 +1,87 @@
+import argparse
+import os
+import shutil
+from training.training import train_networks
+from datetime import datetime
+
+
+def parse_args():
+    desc = "An autoencoder for pose similarity detection"
+
+    parser = argparse.ArgumentParser(description=desc)
+
+    
+    parser.add_argument('--n_classes', type=int, default=2, help='Number of segmented instances in the dataset. Eg. Character and background')
+    parser.add_argument('--n_features', type=int, default=3, help='Number of channels. Eg. 3 for RGB')
+    parser.add_argument('--input_path_dir', type=str, default="/content/drive/MyDrive/GenerativeLabs/dataset/sword_sorcery_data_for_ramiro/paired_data", help='Path root where inputs are located. By default it will contain 3 subfolders: img, inst, label')
+    parser.add_argument('--input_img_dir', type=str, default="02_output", help='Folder name for original images located in input_path_dir')
+    parser.add_argument('--input_label_dir', type=str, default="01_segmented_input", help='Folder name for labeled images located in input_path_dir')        
+    parser.add_argument('--input_inst_dir', type=str, default="01_segmented_input", help='Folder name for instances images located in input_path_dir')    
+    parser.add_argument('--num_epochs', type=int, default=200, help='The number of epochs')   
+    parser.add_argument('--decay_after', type=int, default=200, help='Number of epochs with constant lr')   
+    parser.add_argument('--lr', type=int, default=0.0005, help='Learning rate')   
+    parser.add_argument('--beta_1', type=int, default=0.5, help='Parameter beta_1 for Adam optimizer') 
+    parser.add_argument('--beta_2', type=int, default=0.999, help='Parameter beta_2 for Adam optimizer') 
+    parser.add_argument('--batch_size_1', type=int, default=2, help='The size of batch for step 1 of training.')
+    parser.add_argument('--batch_size_2', type=int, default=1, help='The size of batch for step 2 of training.')
+    parser.add_argument('--target_width_1', type=int, default=1024, help='The size of image for step 1 of training.')
+    parser.add_argument('--target_width_2', type=int, default=1024, help='The size of image for step 1 of training.')
+    parser.add_argument('--experiment_name', type=str, default="", help='A name for the experiment')
+
+
+    """ 
+    parser.add_argument('--input_img_data_dir', type=str, default="Image_X/", help='The directory for CSV input data')
+    parser.add_argument('--output_data_dir', type=str, default="CSV_Y/", help='The directory for CSV input data')
+    parser.add_argument('--base_results_dir', type=str, default="/", help='The base directory to hold the results')
+    parser.add_argument('--output_test_csv_dir', type=str, default="CSV/", help='The directory for result csvs')
+    parser.add_argument('--output_test_graph_dir', type=str, default="Graph/", help='The directory for result csvs')
+    parser.add_argument('--saved_model_dir', type=str, default="Saved_Models/", help='The directory for input data')
+    parser.add_argument('--history_dir', type=str, default="History/", help='The directory for input data')
+    parser.add_argument('--csv_dims', type=int, default=156, help='The number of csv channels')
+    parser.add_argument('--input_channels', type=int, default=2, help='The number of input bone dims')
+    parser.add_argument('--output_channels', type=int, default=2, help='The number of input bone dims')
+    parser.add_argument('--latent_dim', type=int, default=64, help='the size of the latent dim')
+    parser.add_argument('--print_freq', type=int, default=5, help='How often is the status printed')
+    parser.add_argument('--save_freq', type=int, default=10, help='How often is the model saved')
+    parser.add_argument('--save_best_only', action='store_true')
+    parser.add_argument('--print_csv', action='store_true')
+    parser.add_argument('--continue_training', action='store_true')
+    parser.add_argument('--saved_weights_path', type=str, default="N/A", help='the path of the saved weights')
+    parser.add_argument('--notes', type=str, default="N/A", help='A description of the experiment')
+    parser.add_argument('--img_2_bone', action='store_true')
+    """
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    args.experiment_name = datetime.now().strftime("%Y_%m_%d_%H_%M") + "_" + args.experiment_name
+    
+    """
+    args.base_results_dir = os.path.join(args.base_results_dir,args.experiment_name)
+
+    if(not os.path.exists(args.base_results_dir)):
+        os.makedirs(args.base_results_dir)
+        os.makedirs(os.path.join(args.base_results_dir,"CSV"))
+        os.makedirs(os.path.join(args.base_results_dir,"Graph"))
+        os.makedirs(os.path.join(args.base_results_dir,"History"))
+        os.makedirs(os.path.join(args.base_results_dir,"Saved_Models"))
+
+
+    if(args.img_2_bone):
+        normalize_image_data(args)
+        train_img_2_bone(args)
+    else:
+        normalize_data(args)
+        train(args)
+        
+    """
+
+    train_networks(args)
+    print("done training")
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ training/training.py
@@ -0,0 +1,196 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
+from tqdm.auto import tqdm
+import time
+
+from models.discriminators import MultiscaleDiscriminator
+from models.generators import GlobalGenerator, LocalEnhancer
+from models.loss import Loss
+from models.models_utils import Encoder
+from utils.dataloader import SwordSorceryDataset
+from utils.utils import print_device_name, show_tensor_images
+
+# Parse torch version for autocast
+# ######################################################
+version = torch.__version__
+version = tuple(int(n) for n in version.split('.')[:-1])
+has_autocast = version >= (1, 6)
+# ######################################################
+
+def train(dataloader, models, optimizers, schedulers, device, desc, epochs=100):
+    encoder, generator, discriminator = models
+    g_optimizer, d_optimizer = optimizers
+    g_scheduler, d_scheduler = schedulers
+
+    loss_fn = Loss(device=device)
+
+    display_step = 100
+    cur_step = 0
+    mean_g_loss = 0.0
+    mean_d_loss = 0.0
+
+    for epoch in tqdm(range(epochs), desc=desc, leave=True):
+        # Training epoch
+        # time
+        since_load = time.time()
+        for (x_real, labels, insts, bounds) in tqdm(dataloader, desc=f'  inner loop for epoch {epoch}', leave=False):
+            x_real = x_real.to(device)
+            labels = labels.to(device)
+            insts = insts.to(device)
+            bounds = bounds.to(device)
+
+            # time
+            time_elapsed_load = time.time() - since_load
+            since_training = time.time()
+
+            # Enable autocast to FP16 tensors (new feature since torch==1.6.0)
+            # If you're running older versions of torch, comment this out
+            # and use NVIDIA apex for mixed/half precision training
+            if has_autocast:
+                with torch.cuda.amp.autocast(enabled=(device=='cuda')):
+                    g_loss, d_loss, x_fake = loss_fn(
+                        x_real, labels, insts, bounds, encoder, generator, discriminator
+                    )
+            else:
+                g_loss, d_loss, x_fake = loss_fn(
+                    x_real, labels, insts, bounds, encoder, generator, discriminator
+                )
+
+            g_optimizer.zero_grad()
+            g_loss.backward()
+            g_optimizer.step()
+
+            d_optimizer.zero_grad()
+            d_loss.backward()
+            d_optimizer.step()
+
+            mean_g_loss += g_loss.item() / display_step
+            mean_d_loss += d_loss.item() / display_step
+
+            if cur_step % display_step == 0 and cur_step > 0:
+                print('Step {}: Generator loss: {:.5f}, Discriminator loss: {:.5f}'
+                      .format(cur_step, mean_g_loss, mean_d_loss))
+                show_tensor_images(x_fake.to(x_real.dtype))
+                show_tensor_images(x_real)
+                mean_g_loss = 0.0
+                mean_d_loss = 0.0
+            cur_step += 1
+
+            # time 
+            time_elapsed_training = time.time() - since_training
+            since_load = time.time()
+            print('Loading images complete in {:.0f}m {:.0f}s {:.0f}ms'.format(
+            time_elapsed_load // 60, time_elapsed_load % 60, 60*time_elapsed_load % 60))
+            print('Training complete in {:.0f}m {:.0f}s {:.0f}ms'.format(
+            time_elapsed_training // 60, time_elapsed_training % 60, 60*time_elapsed_training % 60))
+
+        g_scheduler.step()
+        d_scheduler.step()
+
+
+def weights_init(m):
+    ''' Function for initializing all model weights '''
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        nn.init.normal_(m.weight, 0., 0.02)
+
+
+def train_networks(args):
+
+    # init params
+    n_classes = args.n_classes                  # total number of object classes
+    rgb_channels = n_features = args.n_features       
+    device = 'cuda'
+    train_dir = [{
+        'path_root': args.input_path_dir, #
+        'path_inputs': [(args.input_img_dir, 'orig_img'), (args.input_inst_dir, 'inst_map'), (args.input_label_dir, 'label_map')]
+        }]
+    epochs = args.num_epochs       # total number of train epochs
+    decay_after = args.decay_after              # number of epochs with constant lr
+    lr = args.lr
+    betas = (args.beta_1, args.beta_2)
+
+    # functions
+    def lr_lambda(epoch):
+        ''' Function for scheduling learning '''
+        return 1. if epoch < decay_after else 1 - float(epoch - decay_after) / (epochs - decay_after)
+
+    ### Init train
+    ## Phase 1: Low Resolution (1024 x 512)
+    dataloader1 = DataLoader(
+        SwordSorceryDataset(train_dir, target_width=args.target_width_1, n_classes=n_classes, n_inputs=3),
+        collate_fn=SwordSorceryDataset.collate_fn, batch_size=args.batch_size_1, shuffle=True, drop_last=False, pin_memory=True,
+    )
+    encoder = Encoder(rgb_channels, n_features).to(device).apply(weights_init)
+    #generator1 = GlobalGenerator(n_classes + n_features + 1, rgb_channels).to(device).apply(weights_init)
+    #discriminator1 = MultiscaleDiscriminator(n_classes + 1 + rgb_channels, n_discriminators=2).to(device).apply(weights_init)
+    #HARCODED BECAUSE LABEL IMAGE!  n_classes=1
+    generator1 = GlobalGenerator(1 + n_features + 1, rgb_channels).to(device).apply(weights_init)
+    discriminator1 = MultiscaleDiscriminator(1 + 1 + rgb_channels, n_discriminators=2).to(device).apply(weights_init)
+
+    g1_optimizer = torch.optim.Adam(list(generator1.parameters()) + list(encoder.parameters()), lr=lr, betas=betas)
+    d1_optimizer = torch.optim.Adam(list(discriminator1.parameters()), lr=lr, betas=betas)
+    g1_scheduler = torch.optim.lr_scheduler.LambdaLR(g1_optimizer, lr_lambda)
+    d1_scheduler = torch.optim.lr_scheduler.LambdaLR(d1_optimizer, lr_lambda)
+
+
+    ## Phase 2: High Resolution (2048 x 1024)
+    dataloader2 = DataLoader(
+        SwordSorceryDataset(train_dir, target_width=args.target_width_1, n_classes=n_classes, n_inputs=3),
+        collate_fn=SwordSorceryDataset.collate_fn, batch_size=args.batch_size_2, shuffle=True, drop_last=False, pin_memory=True,
+    )
+    #generator2 = LocalEnhancer(n_classes + n_features + 1, rgb_channels).to(device).apply(weights_init)
+    #discriminator2 = MultiscaleDiscriminator(n_classes + 1 + rgb_channels).to(device).apply(weights_init)
+    #HARCODED BECAUSE LABEL IMAGE!  n_classes=1
+    generator2 = LocalEnhancer(1 + n_features + 1, rgb_channels).to(device).apply(weights_init)
+    discriminator2 = MultiscaleDiscriminator(1 + 1 + rgb_channels).to(device).apply(weights_init)
+
+    g2_optimizer = torch.optim.Adam(list(generator2.parameters()) + list(encoder.parameters()), lr=lr, betas=betas)
+    d2_optimizer = torch.optim.Adam(list(discriminator2.parameters()), lr=lr, betas=betas)
+    g2_scheduler = torch.optim.lr_scheduler.LambdaLR(g2_optimizer, lr_lambda)
+    d2_scheduler = torch.optim.lr_scheduler.LambdaLR(d2_optimizer, lr_lambda)
+
+    ### Training
+    print_device_name()
+
+    # Phase 1: Low Resolution
+    #######################################################################
+    train(
+        dataloader1,
+        [encoder, generator1, discriminator1],
+        [g1_optimizer, d1_optimizer],
+        [g1_scheduler, d1_scheduler],
+        device,
+        desc='Epoch loop G1',
+    )
+
+
+    # Phase 2: High Resolution
+    #######################################################################
+    # Update global generator in local enhancer with trained
+    generator2.g1 = generator1.g1
+
+    # Freeze encoder and wrap to support high-resolution inputs/outputs
+    def freeze(encoder):
+        encoder.eval()
+        for p in encoder.parameters():
+            p.requires_grad = False
+
+        @torch.jit.script
+        def forward(x, inst):
+            x = F.interpolate(x, scale_factor=0.5, recompute_scale_factor=True)
+            inst = F.interpolate(inst.float(), scale_factor=0.5, recompute_scale_factor=True)
+            feat = encoder(x, inst.int())
+            return F.interpolate(feat, scale_factor=2.0, recompute_scale_factor=True)
+        return forward
+
+    train(
+        dataloader2,
+        [freeze(encoder), generator2, discriminator2],
+        [g2_optimizer, d2_optimizer],
+        [g2_scheduler, d2_scheduler],
+        device,
+        desc='Epoch loop G2',
+    )
