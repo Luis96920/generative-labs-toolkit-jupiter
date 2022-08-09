09ec97d8e30eaa59b73223621cf7ac89c2f45a64
luis96920 <lucasm3938@gmail.com>
2022-08-09 12:06:53 -0300

    Update training.py

--- training/training.py
+++ training/training.py
@@ -235,8 +235,8 @@ def train(dataloader, models, optimizers, schedulers, args, epochs, stage='', de
             mean_g_loss += g_loss.item() / args.write_logs_step
             mean_d_loss += d_loss.item() / args.write_logs_step
             if cur_step % args.write_logs_step == 0 and cur_step > 0:
-                args.writer.add_scalar('Loss Generator', mean_g_loss, cur_step)
-                args.writer.add_scalar('Loss Discriminator', mean_d_loss, cur_step)
+                args.writer.add_scalar(f'Loss Generator {stage}', mean_g_loss, cur_step)
+                args.writer.add_scalar(f'Loss Discriminator {stage}', mean_d_loss, cur_step)
                 #args.writer.add_scalar(f'Epoch {stage}', epoch, time_elapsed_training)
                 mean_g_loss = 0.0
                 mean_d_loss = 0.0
